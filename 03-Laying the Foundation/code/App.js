6e40fa1d5575eaef13f5b9e2d2fb71f9059f3e4c
Dipan <dipanmajumder55@gmail.com>
2025-01-31 01:15:55 +0530

    Add files via upload

---
+++ 03-Laying the Foundation/code/App.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// React.createElement is an object (Js Object) &
+// while rendering on browser these React.createElement(object) is converted to HTMLElement
+
+//! React.createElement ==> ReactElement(JS-Object) ==> HTMLElement(render)
+
+// while creating react element we use core React
+// but while rendering we use ReactDOM for rendering react element on Browser
+
+const heading = React.createElement('h1' , {id:'parent'} , 'Namaste React');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
+
+// If your ROOT HTML element contains child element, THEN react will replace that child element with the react element (NOT APPEND)
+// React.createElement is not a good way for creating React apps ----> hence JSX is created
+
+
+// JSX
+// JSX - is HTML or XML like Syntax (JSX is not HTML in JS)
+const jsxHeading = (<h1 className='heading'>
+                        Namaste React from jsx
+                    </h1>);
+root.render(jsxHeading);
+//! JSX ==>Babel transpiles it to React.createElement ==> ReactElement(JS-Object) ==> HTMLElement(render)
+// JSX (transpiled before it reaches the JS Engine)
+// ReactDOM will not understand JSX Code , Hence jsx code is transpiled before (rendering) / before it reaches root.render
+// Browser can understand Core js Code or ECMASCRIPT6 (Es6) code , hence parcel is transpiling the code.
+// parcel is not doing by itself , it manages for you.
+// Transpiling is doing with the help of one of the Parcel Package called - [Babel]
+// Babel is js compiler (it takes jsx code & converts it to the code that js engine understands)
+// Babel is exactly Transpiling our code
+// Babel will take your jsx code convert it to the react.createElement & now react can understand the code & now react can render the jsx properly
+// transpiled =  your code is converted to the code that browsers can understand / that react can understand
+// At the end of the day , behind the scenes JSX code is transpiled to React.createElement and so on
+// hence jsx & React.createElement are same
+
+
+//! React Functional Components - A js Function that returns a piece of jsx Code or (bunch of jsx code) is React Functional Components
+
+// react Element
+const elem = <span>ReactDOM</span>
+const title = (<h1> {elem} Namaste React using JSX</h1>);
+
+// react Component
+const HeadingComponent = () => {
+    return <h1 className="heading">Namaste Bro</h1>;
+}
+
+const HeadingComponent1 = () => (
+    <h1 className="heading">Namaste Bro 1</h1>
+);
+
+const HeadingComponent2 = () => <h1 className="heading">Namaste Bro 2</h1>
+
+const HeadingComponent3 = function(){
+    return(
+        <h1 className="heading">Namaste Bro 3</h1>
+    )
+}
+
+let numb = 10000;
+
+const HeadingComponent4 = () => {
+    return (
+      <div id="container">
+        <HeadingComponent />
+        {numb}
+        <HeadingComponent1 />
+        {HeadingComponent2()}
+        <HeadingComponent3></HeadingComponent3>
+        <h1 className="heading">Namaste Bro 4 + {numb} = {4 + numb}</h1>
+        {title}
+      </div>
+    )
+};
+
+// rendering a component
+root.render(<HeadingComponent4 />);
